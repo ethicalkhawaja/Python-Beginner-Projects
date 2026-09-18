cda76313207632cd61f1861075ba56ce1c51a4ce
Mohammad Omar Khawaja Zada <omar610710@gmail.com>
2026-09-18 19:02:44 +0430

    Create rock_paper_scissors.py

---
+++ 02-rock-paper-scissors/rock_paper_scissors.py
@@ -0,0 +1,26 @@
+import random
+
+player = input("Choose rock, paper, or scissors")
+
+computer = random.randint(1, 3)
+
+if computer == 1:
+    computer = "rock"
+elif computer == 2:
+    computer = "paper"
+else:
+    computer = "scissors"
+
+print("You chose:", player)
+print("Computer chose:", computer)
+
+if player == computer:
+    print("It's a tie!")
+elif player == "rock" and computer == "scissors":
+    print("You win!")
+elif player == "paper" and computer == "rock":
+    print("You win!")
+elif player == "scissors" and computer == "paper":
+    print("You win!")
+else:
+    print("You lose!")
