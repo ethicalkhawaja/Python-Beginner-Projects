0536a2192b5de90eb31d57a12c794bf6fdd06a17
Mohammad Omar Khawaja Zada <omar610710@gmail.com>
2026-09-18 17:39:04 +0430

    Add number guessing game

--- 01-Number-guessing/number_guessing.py
+++ 01-Number-guessing/number_guessing.py
@@ -1,15 +1,18 @@
 import random
-secret_num = random.randint(1,15)
-for guesses in range(1,6):
-    guess = int(input("Enter your guess:"))
-    if guess < secret_num:
-        print("its low my friend")
-    elif guess > secret_num:
-        print("its higher.")
-    else:
-        break
-if guess == secret_num:
-    print("Thats it you did well here it took",guesses, "guesses and you made it")  
-else:
-    print("You couldn't guessed correctly so the correct guess is", secret_num)        
 
+secret_num = random.randint(1, 15)
+
+for guesses in range(1, 6):
+    guess = int(input("Enter your guess: "))
+
+    if guess < secret_num:
+        print("It's low, my friend.")
+    elif guess > secret_num:
+        print("It's higher.")
+    else:
+        print("That's it! You did well.")
+        print("It took", guesses, "guesses and you made it!")
+        break
+else:
+    print("You couldn't guess correctly.")
+    print("The correct guess is", secret_num)
